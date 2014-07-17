ed24d88766db8cc415c266bbd73d3d2cb84dbca7
John Firebaugh <john.firebaugh@gmail.com>
2014-07-17 14:38:08 -0700

    Add compare/deploy scripts

---
+++ bin/deploy_results.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+aws s3 cp tests/ s3://mapbox-gl-testing/headless/$TRAVIS_REPO_SLUG/$TRAVIS_JOB_NUMBER/ --acl public-read --recursive
+
+echo http://mapbox-gl-testing.s3.amazonaws.com/headless/$TRAVIS_REPO_SLUG/$TRAVIS_JOB_NUMBER/index.html
