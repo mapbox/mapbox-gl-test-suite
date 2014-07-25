04f60c50e2515d62ae1e85ca074cc0874a9d3b6f
John Firebaugh <john.firebaugh@gmail.com>
2014-07-24 17:58:27 -0700

    Less verbosity

--- bin/deploy_results.sh
+++ bin/deploy_results.sh
@@ -3,6 +3,6 @@
 set -e
 set -o pipefail
 
-aws s3 cp tests/ s3://mapbox-gl-testing/headless/$TRAVIS_REPO_SLUG/$TRAVIS_JOB_NUMBER/ --acl public-read --recursive
+aws s3 cp tests/ s3://mapbox-gl-testing/headless/$TRAVIS_REPO_SLUG/$TRAVIS_JOB_NUMBER/ --acl public-read --recursive > /dev/null
 
 echo http://mapbox-gl-testing.s3.amazonaws.com/headless/$TRAVIS_REPO_SLUG/$TRAVIS_JOB_NUMBER/index.html
