5cc4f9cdbeada9511f81c88e87423e9fe365451d
Konstantin Käfer <mail@kkaefer.com>
2014-11-05 11:32:26 +0100

    read port number from argument

--- bin/server.py
+++ bin/server.py
@@ -11,12 +11,13 @@ import threading
 # Change to the correct path in case the server was launched differently.
 os.chdir(path.realpath(path.join(path.dirname(__file__), '..')))
 
+port = int(sys.argv[1] if 1 in sys.argv else 2900)
 
 Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 class WebServer(SocketServer.TCPServer):
     allow_reuse_address = True
 
-server = WebServer(('localhost', 2900), Handler)
+server = WebServer(('localhost', port), Handler)
 
 def signal_handler(signal, frame):
     threading.Thread(target = server.shutdown).start()
@@ -24,6 +25,6 @@ def signal_handler(signal, frame):
 signal.signal(signal.SIGHUP, signal_handler)
 signal.signal(signal.SIGINT, signal_handler)
 
-print 'Starting HTTP server on port 2900'
+print 'Starting HTTP server on port %d' % port
 server.serve_forever()
-print 'Stopped HTTP server on port 2900'
+print 'Stopped HTTP server on port %d' % port
